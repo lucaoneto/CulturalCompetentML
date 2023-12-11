8e72a29eda3f90ac6a90ce05369dd6c27f4d64b7
EnzoUbaldoPetrocco <48513075+EnzoUbaldoPetrocco@users.noreply.github.com>
2023-12-11 08:51:37 +0100

    Add files via upload
    added Shallow Learning

---
+++ CultureCompetence/standard/0.1/rf/rf_tur.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.insert(1, '../../../')
+
+from standard.classificator import ClassificatorClass
+from standard.strings import Strings
+
+strings = Strings()
+paths = strings.paths
+
+cc = ClassificatorClass(2,1,paths,'RFC',30, fileName='rf_tur.csv')
+cc.executenineone()
